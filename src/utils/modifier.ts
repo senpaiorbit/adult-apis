a46e00c0ca7dde971bae15db5d7dfcbc994df720
tanbirst1st1 <senpaiorbit@gmail.com>
2026-03-08 14:55:14 +0530

    Add files via upload

--- src/utils/modifier.ts
+++ src/utils/modifier.ts
@@ -16,7 +16,7 @@ export function timeAgo(input: Date): string | undefined {
   const formatter: any = new Intl.RelativeTimeFormat("en");
   const ranges: Record<string, number> = {
     years: 3600 * 24 * 365, months: 3600 * 24 * 30,
-    weeks: 3600 * 24 * 7,   days:   3600 * 24,
+    weeks: 3600 * 24 * 7,   days: 3600 * 24,
     hours: 3600,             minutes: 60, seconds: 1,
   };
   const elapsed = (date.getTime() - Date.now()) / 1000;
